6da0059790fe11fee8e1040d87195b7385cf6034
cpasion-office-win10 <373704015@qq.com>
2022-04-27 18:06:43 +0800

    perf(index.mjs): 修改入口位置为 /index.mjs

---
+++ src/commands/uploadImg.mjs
@@ -0,0 +1,13 @@
+/*!
+ * @Author: CPS
+ * @email: 373704015@qq.com
+ * @Date: 2022-04-27 18:03:57.821912
+ * @Last Modified by: CPS
+ * @Last Modified time: 2022-04-27 18:03:57.821912
+ * @Projectname
+ * @file_path "W:\CPS\MyProject\demo\cps-cli\cps-cli\src\commands"
+ * @Filename "uploadImg.mjs"
+ * @Description: 功能描述
+ */
+
+"use strict";
