b1470d3d307e42ea5b4ca661d5bad24c0fde4a23
CPS <holy.dandelion@139.com>
2022-05-04 00:22:02 +0800

    feat(upload.mjs): 完成typora上传功能，本地部分，上传图片后复制到本地，同时调用git推送到远程

--- src/commands/upload.mjs
+++ src/commands/upload.mjs
@@ -9,17 +9,61 @@
  * @Filename "upload.mjs"
  * @Description: 功能描述
  */
+
+import { resolve, basename } from "path";
 import { log } from "console";
 
 import fse from "fs-extra";
 
+function Exit(code) {
+  return process.exit(code);
+}
+
 export default async ctx => {
   const temp_path = "d:\\@cps-cli.log";
   const imgPathList = ctx.argv;
+  const config = ctx.configManager.config["upload"];
 
-  await fse.ensureFile(temp_path);
-  await fse.writeFile(temp_path, imgPathList.toString());
+  // await fse.ensureFile(temp_path);
+  // await fse.writeFile(temp_path, imgPathList.toString());
+  switch (config["handler"]) {
+    case "local":
+      // statements_1
 
-  log("upload done ! 123123");
+      const uploadConfig = config["local"];
+      const cwd = uploadConfig["path"];
+      if (!fse.lstatSync(cwd)) Exit(0);
+
+      let shellResult;
+      shellResult = await ctx.shell(["git", "pull", "origin", "master"], { cwd });
+      console.log("git pull: ", shellResult);
+      if (!shellResult.success) Exit(0);
+
+      // imgPathList.forEach(imgPath => {
+      //   const src = resolve(imgPath);
+      //   const dest = resolve(cwd, basename(imgPath));
+
+      //   if (fse.lstatSync(src)) fse.copySync(src, dest);
+      //   console.log(dest);
+      // });
+
+      shellResult = await ctx.shell(["git", "add", "."], { cwd });
+      console.log("git add: ", shellResult);
+      shellResult = await ctx.shell(["git", "commit", "-m", "cps-cli-upload"], { cwd });
+      console.log("git commit: ", shellResult);
+      shellResult = await ctx.shell(["git", "push", "origin", "master"], { cwd });
+      console.log("git push: ", shellResult);
+
+      Exit(0);
+      break;
+    case "picgo":
+
+    default:
+      return;
+      break;
+  }
+
+  // 复制图片到本地路径
+
   return imgPathList;
 };
